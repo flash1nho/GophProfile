47001dae93388db55b757a4be1934a4b4b9ecb24
Igor Okunev <okunev1988@gmail.com>
2026-05-12 17:53:42 +0500

    fix logs

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -13,21 +13,11 @@ func New() (*zap.Logger, error) {
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
 	encoder := zapcore.NewJSONEncoder(encoderConfig)
-
 	stdout := zapcore.AddSync(os.Stdout)
-
-	file, err := os.OpenFile("/logs/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
-	}
-
-	fileWriter := zapcore.AddSync(file)
-
 	level := zapcore.InfoLevel
 
 	core := zapcore.NewTee(
 		zapcore.NewCore(encoder, stdout, level),
-		zapcore.NewCore(encoder, fileWriter, level),
 	)
 
 	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
